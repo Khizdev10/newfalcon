7ae3e68ffeed23d581924b698d79da0d73784def
Khizar Abbasi <khizdev10@gmail.com>
2025-12-23 23:00:53 +0500

    ui-fixes

--- app/components/contact-btns.tsx
+++ app/components/contact-btns.tsx
@@ -10,12 +10,15 @@ export default function Btns () {
                 className="shine-btn-c fixed bottom-6 right-6 bg-green-500  border-2 border-green-200 hover:bg-green-600 text-white p-2 rounded-full shadow-lg flex items-center justify-center z-50 transition-all"
             >
                 <FaWhatsapp className="text-3xl" />
-
+                     {/* this is 
+                     the website for
+                     my mamu business */}
             </a>
             <a
                 href="tel:+923442656567"
                 className="shine-btn-c fixed bottom-24 right-6 bg-blue-600 border-2 border-blue-200 hover:bg-blue-700 text-white p-3 rounded-full shadow-lg flex items-center justify-center z-50 transition-all"
             >
+
                 <FaPhone className="text-2xl" />
             </a>
 
